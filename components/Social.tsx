510ffc5024ea4db9aca67a77d5c4de401333134f
theBashShell <edohbruno@gmail.com>
2019-11-21 11:04:19 +0000

    social fixed and snapchat added

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,66 +1,98 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react';
+import { notification } from 'antd';
 
-const style = <style jsx>{`
-.social {
-    flex-flow: row wrap;
-  }
-
-  .social img {
-    width: 40px;  
-    transition: all 0.5s ease 0.1s;
-    padding: 7px 7px;
-    padding-top: 18px;
-  }
-
-  .social img:hover {
-    transform: scale(1.3);
-    filter: grayscale(1);
-    transition: all 0.5s ease 0.1s;
-  }
-
-  @media (min-width: 700px) {
+const style = (
+  <style jsx>{`
     .social {
-      position: fixed;
-      left: 22px;
-      flex-flow: column nowrap;
-      top: 30%;
+      flex-flow: row wrap;
     }
 
     .social img {
-      padding-top: 5px; 
+      width: 40px;
+      transition: all 0.5s ease 0.1s;
+      padding: 7px 7px;
+      padding-top: 18px;
+      filter: grayscale(1);
     }
-  }
-`}</style>
+
+    .ant-popover-inner-content {
+      padding: 4px;
+    }
+
+    .social img:hover {
+      transform: scale(1.3);
+      filter: unset;
+      transition: all 0.5s ease 0.1s;
+    }
+
+    @media (min-width: 700px) {
+      .social {
+        position: fixed;
+        left: 22px;
+        flex-flow: column nowrap;
+        top: 30%;
+      }
+
+      .social img {
+        padding-top: 5px;
+      }
+    }
+  `}</style>
+);
+
+const snapchat =   (
+  <div>
+    <img
+      src="/images/snapchat.jpg"
+      alt="snapchat"
+      style={{ width: '300px' }}
+    />
+  </div>
+);
 
 function Social() {
-    return (
-        <>
-        <div className="social centered">
-<a href="https://github.com/theBashShell" target="_blank">
-  <img src="https://api.iconify.design/logos:github-icon.svg" />
-</a>
-<a href="https://www.linkedin.com/in/bruno-edoh/" target="_blank">
-  <img src="https://api.iconify.design/ps:linkedin.svg" />
-</a>
-<a href="https://twitter.com/theBashShell" target="_blank">
-  <img src="https://api.iconify.design/dashicons:twitter.svg" />
-</a>
-<a
-  href="https://stackoverflow.com/users/6790436/thebashshell"
-  target="_blank"
->
-  <img src="https://api.iconify.design/simple-icons:stackoverflow.svg" />
-</a>
-<a href="https://www.instagram.com/thebashshell/" target="_blank">
-  <img src="https://api.iconify.design/ion:logo-instagram.svg" />
-</a>
-<a href='https://codesandbox.io/u/theBashShell' target="_blank">
-<img src='https://api.iconify.design/logos-codesandbox.svg' />
-</a>
-</div>
-{style}
-        </>
-    )
+
+  const handleSnapChat = (event: SyntheticEvent) => {
+    event.preventDefault();
+    notification.destroy();
+    notification.open({
+      message: '',
+      description: snapchat,
+      duration: 20
+    })
+  };
+
+  
+
+  return (
+    <div className="social centered">
+      <a href="https://github.com/theBashShell" target="_blank">
+        <img src="https://image.flaticon.com/icons/svg/733/733553.svg" />
+      </a>
+      <a href="https://www.linkedin.com/in/bruno-edoh/" target="_blank">
+        <img src="https://image.flaticon.com/icons/svg/2111/2111499.svg" />
+      </a>
+      <a href="https://twitter.com/theBashShell" target="_blank">
+        <img src="https://image.flaticon.com/icons/svg/733/733579.svg" />
+      </a>
+      <a
+        href="https://stackoverflow.com/users/6790436/thebashshell"
+        target="_blank"
+      >
+        <img src="https://image.flaticon.com/icons/svg/2111/2111628.svg" />
+      </a>
+      <a href="https://www.instagram.com/thebashshell/" target="_blank">
+        <img src="https://image.flaticon.com/icons/svg/174/174855.svg" />
+      </a>
+      <a href="https://codesandbox.io/u/theBashShell" target="_blank">
+        <img src="https://api.iconify.design/simple-icons:codesandbox.svg?color=teal" />
+      </a>
+      <a href="#" onClick={handleSnapChat}>
+        <img src="https://image.flaticon.com/icons/svg/2111/2111616.svg" />
+      </a>
+      {style}
+    </div>
+  );
 }
 
-export default Social
+export default Social;
